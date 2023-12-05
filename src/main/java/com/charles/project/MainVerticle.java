efb1f451a88acfa69c6ae98f3e5f3ddad4e72abb
dev-gandalf <125443743+dev-gandalf@users.noreply.github.com>
2023-12-05 15:21:27 +0100

    Add files via upload

---
+++ src/main/java/com/charles/project/MainVerticle.java
@@ -0,0 +1,123 @@
+package com.charles.project;
+
+
+import java.nio.file.FileAlreadyExistsException;
+import java.util.*;
+import com.charles.project.model.Product;
+import com.charles.project.service.ReadWriteToDisk;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+
+
+
+public class MainVerticle extends AbstractVerticle {
+
+	@Override
+	public void start() {
+
+// Initialize router
+		Router router = Router.router(vertx);
+		router.route().handler(BodyHandler.create());
+
+		
+//Create HTTP server
+		vertx.createHttpServer().requestHandler(router).listen(8000);
+
+		
+		
+// Controllers
+		
+		router.get("/product/list").handler(this::getProducts);
+
+		router.post("/product/create").handler(this::createProduct);
+
+		router.delete("/product/delete/:name").handler(this::deleteProduct);
+
+		
+	}
+
+// Controller methods	
+	
+	public void getProducts(RoutingContext ctx) {
+
+		ReadWriteToDisk reader = new ReadWriteToDisk();
+		List<Product> productList = new ArrayList<>();
+		productList = reader.getAllProdcuts();
+
+		ctx
+		.response()
+		.putHeader("content-type", "application/json; charset=utf-8")
+		.setStatusCode(200)
+		.end(Json.encodePrettily(productList));
+	}
+
+	
+	
+	public void createProduct(RoutingContext ctx) {
+
+		String json = ctx.body().asString();
+		ObjectMapper objectMapper = new ObjectMapper();
+		Product product;
+		ReadWriteToDisk writer = new ReadWriteToDisk();
+		try {
+			product = objectMapper.readValue(json, Product.class);
+			writer.createProduct(product);
+			
+			ctx
+			.response()
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.setStatusCode(200)
+			.end("Product Successfully created");
+		
+		}catch (FileAlreadyExistsException e) {
+			e.printStackTrace();
+			
+			ctx
+			.response()
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.setStatusCode(200)
+			.end("Product already exists");
+		}catch (Exception e) {
+			e.printStackTrace();
+			
+			ctx
+			.response()
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.setStatusCode(200)
+			.end("Something went wrong");
+		}
+	}
+	
+	
+	
+	public void deleteProduct(RoutingContext ctx){
+		String productName = ctx.pathParam("name");
+		ReadWriteToDisk delete = new ReadWriteToDisk();
+		try {
+			delete.deleteProduct(productName);
+			ctx
+			.response()
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.setStatusCode(200)
+			.end("Product successfully deleted");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+			ctx
+			.response()
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.setStatusCode(200)
+			.end("Product not found");
+		}
+		
+		
+		
+		
+	}
+
+}
---
+++ src/main/java/com/charles/project/service/ReadWriteToDisk.java
@@ -0,0 +1,86 @@
+package com.charles.project.service;
+
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.charles.project.model.Product;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+
+public class ReadWriteToDisk {
+
+	//Reminder: Use Path class instead of String
+	private String destinationPath = System.getProperty("user.home") + "/Products";
+
+	
+	public ReadWriteToDisk() {
+		super();
+	}
+
+	
+	
+	public void createProduct(Product product) throws Exception {
+
+		File createDir = new File(destinationPath + "/" + product.getName());
+
+		if (createDir.exists()) {
+			throw new FileAlreadyExistsException(createDir.getAbsolutePath());
+		} else {
+			createDir.mkdirs();
+
+			File createFile = new File(destinationPath + "/" + product.getName() + "/info.txt");
+			createFile.createNewFile();
+
+			Writer writer = new FileWriter(createFile.getAbsolutePath());
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(product, writer);
+			writer.flush(); // flush data to file <---
+			writer.close(); // close write <---
+		}
+
+	}
+
+	public List<Product> getAllProdcuts() {
+		List<Product> productList = new ArrayList<>();
+		Gson gson = new Gson();
+		JsonReader reader;
+		File file = new File(destinationPath);
+		File[] fileList = file.listFiles();
+		for (File fl : fileList) {
+
+			try {
+				reader = new JsonReader(new FileReader(fl.getAbsolutePath() + "/info.txt"));
+				Product product = gson.fromJson(reader, Product.class);
+				productList.add(product);
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		return productList;
+
+	}
+
+	
+	public void deleteProduct(String productName) throws FileNotFoundException {
+		File deleteDir = new File(destinationPath + "/" + productName);
+		if (deleteDir.exists()) {
+			File[] allContents = deleteDir.listFiles();
+			if (allContents != null) {
+				for (File file : allContents) {
+					file.delete();
+				}
+			}
+			deleteDir.delete();
+
+		} else {
+			throw new FileNotFoundException();
+		}
+
+	}
+}
